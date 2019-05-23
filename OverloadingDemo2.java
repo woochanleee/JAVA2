acd46facc46a321b65d793b3090cee7e5f6ae4d5
woochanleee <030219woo@naver.com>
2019-05-23 23:50:23 +0900

    lecture_4

--- OverloadingDemo2.java
+++ OverloadingDemo2.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
-public class OverloadingDemo2 {
+public class OverloadingDemo2 extends OverloadingDemo{
+    void A (String arg1, String arg2){System.out.println("sub class : void A (String arg1, String arg2)");}
+    void A (){System.out.println("sub class : void A ()");}
+    public static void main(String[] args) {
+        OverloadingDemo2 od = new OverloadingDemo2();
+        od.A();
+        od.A(1);
+        od.A("coding everybody");
+        od.A("coding everybody", "coding everybody");
+    }
 }
