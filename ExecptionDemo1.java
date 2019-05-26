bba7c9117a542157309d33189e90fb1cd3694932
woochanleee <030219woo@naver.com>
2019-05-26 10:20:52 +0900

    lecture_9

--- ExecptionDemo1.java
+++ ExecptionDemo1.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.concurrent.ExecutionException;
 
-public class ExecptionDemo1 {
+class A{
+        private int[] arr = new int[3];
+        A(){
+            arr[0] = 0;
+            arr[1] = 10;
+            arr[2] = 20;
+        }
+            public void z(int first, int second){
+                try{
+                System.out.println(arr[first] / arr[second]);
+            } catch (ArithmeticException e){
+                    System.out.println("ArithmeticException e");
+            } catch (ArrayIndexOutOfBoundsException e){
+                    System.out.println("ArrayIndexOutOfBoundsException e");
+            } catch (Exception e){
+                    System.out.println("Exception e");
+            } finally {
+                    System.out.println("finally");
+            }
+        }
+    }
+
+    public class ExecptionDemo1 {
+        public static void main(String[] args) {
+            A a = new A();
+            a.z(1, 0);
+            a.z(1, 0);
+            a.z(2,1);
+        }
 }
