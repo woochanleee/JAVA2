2a21a0075f29593984c9f1da2401fe47c9de8682
woochanleee <030219woo@naver.com>
2019-05-26 17:17:46 +0900

    lecture_12

---
+++ collection/SetDemo.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class SetDemo {
+
+    public static void main(String[] args) {
+        HashSet<Integer> A = new HashSet<Integer>();
+        A.add(1);
+        A.add(2);
+        A.add(3);
+
+        HashSet<Integer> B = new HashSet<Integer>();
+        A.add(1);
+        A.add(2);
+        A.add(3);
+
+        HashSet<Integer> A = new HashSet<Integer>();
+        A.add(1);
+        A.add(2);
+        A.add(3);
+    }
+}
