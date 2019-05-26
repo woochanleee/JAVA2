f2ae0181aec993ea29c62ad0540ad023c6047f8b
woochanleee <030219woo@naver.com>
2019-05-26 17:18:02 +0900

    lecture_12

--- collection/SetDemo.java
+++ collection/SetDemo.java
@@ -1,22 +1,31 @@
+package collection;
+
 import java.util.HashSet;
 import java.util.Iterator;
 
 public class SetDemo {
 
     public static void main(String[] args) {
-        HashSet<Integer> A = new HashSet<Integer>();
+        HashSet<Integer>  A= new HashSet<Integer>();
         A.add(1);
+        A.add(3);
         A.add(2);
+        A.add(3);
         A.add(3);
 
         HashSet<Integer> B = new HashSet<Integer>();
-        A.add(1);
-        A.add(2);
-        A.add(3);
+        B.add(4);
+        B.add(5);
+        B.add(6);
 
-        HashSet<Integer> A = new HashSet<Integer>();
-        A.add(1);
-        A.add(2);
-        A.add(3);
+        HashSet<Integer> C = new HashSet<Integer>();
+        C.add(7);
+        C.add(8);
+        C.add(9);
+
+        Iterator hi = (Iterator) A.iterator();
+        while(hi.hasNext()){
+            System.out.println(hi.next());
+        }
     }
 }
